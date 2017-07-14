71a506a8aaa03113d952af39a4d84b8a872b56b7
NathanChiu <nathan.chiu94@gmail.com>
2017-07-14 14:18:15 -0700

    Rearrange draw electrode so that it can draw for dimensions smaller than 1.0

--- PoisFFT/src/testpoisson.cc
+++ PoisFFT/src/testpoisson.cc
@@ -26,6 +26,7 @@ https://arxiv.org/pdf/1208.0901.pdf
 
 const double pi = 3.14159265358979323846;
 
+#define EPS0 8.85418782e-12
 #define Q_E 1.6e-19
 #define MAXERROR 1e-3
 #define IND(i,j,k) (i)*(ns[1]*ns[2])+(j)*(ns[2])+k
@@ -60,35 +61,35 @@ Electrodes::~Electrodes( void ){}
 
 void Electrodes::draw(const int ns[3], const double ds[3], const double Ls[3], double* RHS, std::pair<int,double> *electrodemap){
   int i, j, k; //draw the electrode into an electrode map
-  for(int i = (int) x[0]/Ls[0]*ns[0]; i < (int) x[1]/Ls[0]*ns[0]; i++){ //set RHS 0 inside electrodes
-    for(int j = (int) y[0]/Ls[1]*ns[1]; j < (int) y[1]/Ls[1]*ns[1]; j++){
-      for(int k = (int) z[0]/Ls[2]*ns[2]; k < (int) z[1]/Ls[2]*ns[2]; k++){
+  for(int i = (int) ns[0]*x[0]/Ls[0]; i < (int) ns[0]*x[1]/Ls[0]; i++){ //set RHS 0 inside electrodes
+    for(int j = (int) ns[1]*y[0]/Ls[1]; j < (int) ns[1]*y[1]/Ls[1]; j++){
+      for(int k = (int) ns[2]*z[0]/Ls[2]; k < (int) ns[2]*z[1]/Ls[2]; k++){
         RHS[i*ns[1]*ns[2] + j*ns[2] + k] = 0;
       }
     }
   }
   for( int iter = 0; iter < 2; iter++){
-    i = (int) x[iter]/Ls[0]*ns[0]; //xmin first, then xmax
-    for(int j = (int) y[0]/Ls[1]*ns[1]; j <= (int) y[1]/Ls[1]*ns[1]; j++){
-      for(int k = (int) z[0]/Ls[2]*ns[2]; k <= (int) z[1]/Ls[2]*ns[2]; k++){
+    i = (int) ns[0]*x[iter]/Ls[0]; //xmin first, then xmax
+    for(int j = (int) ns[1]*y[0]/Ls[1]; j <= (int) ns[1]*y[1]/Ls[1]; j++){
+      for(int k = (int) ns[2]*z[0]/Ls[2]; k <= (int) ns[2]*z[1]/Ls[2]; k++){
         electrodemap[i*ns[1]*ns[2] + j*ns[2] + k].first = true; //set true for electrode surface.
         electrodemap[i*ns[1]*ns[2] + j*ns[2] + k].second = potential; //set electrode potential
       }
     }
   }
   for( int iter = 0; iter < 2; iter++){
-    j = (int) y[iter]/Ls[1]*ns[1]; //ymin first, then ymax
-    for(int i = (int) x[0]/Ls[0]*ns[0]; i <= (int) x[1]/Ls[0]*ns[0]; i++){
-      for(int k = (int) z[0]/Ls[2]*ns[2]; k <= (int) z[1]/Ls[2]*ns[2]; k++){
+    j = (int) ns[1]*y[iter]/Ls[1]; //ymin first, then ymax
+    for(int i = (int) ns[0]*x[0]/Ls[0]; i <= (int) ns[0]*x[1]/Ls[0]; i++){
+      for(int k = (int) ns[2]*z[0]/Ls[2]; k <= (int) ns[2]*z[1]/Ls[2]; k++){
         electrodemap[i*ns[1]*ns[2] + j*ns[2] + k].first = true; //set true for electrode surface.
         electrodemap[i*ns[1]*ns[2] + j*ns[2] + k].second = potential; //set electrode potential
       }
     }
   }
   for( int iter = 0; iter < 2; iter++){
-    k = (int) z[iter]/Ls[1]*ns[1]; //zmin
-    for(int i = (int) x[0]/Ls[0]*ns[0]; i <= (int) x[1]/Ls[0]*ns[0]; i++){
-      for(int j = (int) y[0]/Ls[1]*ns[1]; j <= (int) y[1]/Ls[1]*ns[1]; j++){
+    k = (int) ns[2]*z[iter]/Ls[2]; //zmin
+    for(int i = (int) ns[0]*x[0]/Ls[0]; i <= (int) ns[0]*x[1]/Ls[0]; i++){
+      for(int j = (int) ns[1]*y[0]/Ls[1]; j <= (int) ns[1]*y[1]/Ls[1]; j++){
         electrodemap[i*ns[1]*ns[2] + j*ns[2] + k].first = true; //set true for electrode surface.
         electrodemap[i*ns[1]*ns[2] + j*ns[2] + k].second = potential; //set electrode potential
       }
@@ -104,7 +105,7 @@ void apply_correction(const int[], const double[], double*, double*, std::pair<i
 
 int main(void){
   std::cout << "Modified by Nathan Chiu. Code is offered as is, with no warranty. See LICENCE.GPL for licence info." << std::endl;
-  const double Ls[3] = {10.0, 10.0, 10.0}; //x, y, z domain dimensions
+  const double Ls[3] = {1.0, 1.0, 1.0}; //x, y, z domain dimensions
   const int ns[3] = {100, 100, 100}; //x, y, z gridpoint numbers
   double ds[3];  // distances between gridpoints
   double cycleErr;
@@ -119,25 +120,35 @@ int main(void){
   double *RHS = new double[ns[0]*ns[1]*ns[2]]; // from which you can get a pointer to contiguous buffer
   double *correction = new double[ns[0]*ns[1]*ns[2]]; // correction used to update RHS
   std::pair<int,double> *electrodemap = new std::pair<int,double>[ns[0]*ns[1]*ns[2]]; //stores electrode surface info and potentials.
+  int cycleCount = 0;
   // ProfilerStart("./profileresult.out"); //using google performance tools
   const std::clock_t begin_time = std::clock();
   init_rhs(ns, ds, Ls, RHS); // set the right-hand side
-  Electrodes elec1(3.0, 6.0, 3.0, 6.0, 3.0, 6.0, 10);
+  Electrodes elec1(0.2, 0.4, 0.2, 0.4, 0.3, 0.6, 5);
+  Electrodes elec2(0.2, 0.4, 0.6, 0.8, 0.3, 0.6, 10);
+  Electrodes elec3(0.6, 0.8, 0.2, 0.4, 0.3, 0.6, 15);
+  Electrodes elec4(0.6, 0.8, 0.6, 0.8, 0.3, 0.6, 20);
   elec1.draw(ns, ds, Ls, RHS, electrodemap);
+  elec2.draw(ns, ds, Ls, RHS, electrodemap);
+  elec3.draw(ns, ds, Ls, RHS, electrodemap);
+  elec4.draw(ns, ds, Ls, RHS, electrodemap); //separately call draw for each electrode.
   PoisFFT::Solver<3, double> S(ns, Ls, BCs); // create solver object, 3 dimensions, double precision
   std::cout << "Beginning solver" << std::endl;
+  save_file2D(ns, ds, Ls, RHS, RHOFILE);
   do{
     S.execute(arr, RHS); //run the solver, can be run many times for different right-hand side
     cycleErr = check_error(ns, ds, arr, correction, electrodemap);
-    std::cout << cycleErr << std::endl;
     apply_correction(ns, ds, RHS, correction, electrodemap);
+    if(cycleCount%10==0){
+      std::cout << "On cycle " << cycleCount << " with error " << cycleErr << "." << std::endl;
+    }
+    cycleCount++;
   }while(cycleErr > MAXERROR);
-  std::cout << "Finished!" << std::endl;
+  std::cout << "Finished on cycle " << cycleCount << " with error " << cycleErr << std::endl;
   save_file2D(ns, ds, Ls, RHS, RHOFILE);
   save_file2D(ns, ds, Ls, arr, CORRECTIONFILE);
   save_file2D(ns, ds, Ls, arr, FILENAME); //solution is in arr
   // ProfilerStop();
-  //check_solution(ns, ds, Ls, arr); // check correctness (compares with known, exact solution)
   std::cout << "Time elapsed: " << float(clock()-begin_time)/CLOCKS_PER_SEC << " seconds" << std::endl;
   std::cout << "Ending, deleting variables" << std::endl;
   delete[] RHS;
@@ -159,13 +170,12 @@ double check_error(const int ns[3], const double ds[3], double *arr, double *cor
   for(int i = 0; i < ns[0]*ns[1]*ns[2]; i++){
     if(electrodemap[i].first == true){ //only check error at electrodes.
       correction[i] = electrodemap[i].second-arr[i]; //intended potential - found potential.
-      correction[i] = 10*correction[i];
+      correction[i] = 1000*correction[i];
       err = std::max(err, fabs((electrodemap[i].second-arr[i])/electrodemap[i].second)); //get largest error value.
     }
   }
   return err;
 }
-
 
 void save_file2D(const int ns[3], const double ds[3], const double Ls[3], double* arr, char fname[]){
   std::ofstream outfile;
@@ -181,31 +191,6 @@ void save_file2D(const int ns[3], const double ds[3], const double Ls[3], double
   }
 }
 
-void check_solution(const int ns[3], const double ds[3], const double Ls[3], double* a){
-  //if the closed form solution is known, then check the obtained solution for correctness
-  int i,j,k;
-  double sum = 0;
-  std::cout << "Checking solution" << std::endl;
-  for (i=0;i<ns[0];i++){
-    for (j=0;j<ns[1];j++){
-      for (k=0;k<ns[2];k++){
-        double x = ds[0]*(i+0.5);
-        double y = ds[1]*(j+0.5);
-        double z = ds[2]*(k+0.5);
-        sum += pow(a[IND(i,j,k)] -
-            (-1.0/(pow(3*pi/Ls[0],2.0) + pow(5*pi/Ls[1],2.0) + pow(7*pi/Ls[2],2.0)) *
-            sin(3*pi*x/Ls[0])*sin(5*pi*y/Ls[1])*sin(7*pi*z/Ls[2])),2.0);
-      }
-    }
-  }
-  if (sum < 1e-10) {
-    std::cout << "OK, converged\n" << std::endl;
-  }
-  else {
-    std::cout << "FAIL, residuum, did not converge." << sum << std::endl;
-  }
-}
-
 void init_rhs(const int ns[3], const double ds[3], const double Ls[3], double* a){
   int i,j,k;
   std::cout << "Initialising RHS" << std::endl;
@@ -215,7 +200,9 @@ void init_rhs(const int ns[3], const double ds[3], const double Ls[3], double* a
       double y = ds[1]*(j+0.5);
       for (k=0;k<ns[2];k++){
         double z = ds[2]*(k+0.5);
-        a[IND(i,j,k)] = 1.5e10*Q_E; //Set default set to bulk volume charge density.
+        // a[IND(i,j,k)] = -1.5e10*Q_E/EPS0; //Set default set to bulk volume charge density.
+        // a[IND(i,j,k)] = -1.5*Q_E/EPS0; //Set default set to bulk volume charge density.
+        a[IND(i,j,k)] = -1.5*Q_E/EPS0; //Set default set to bulk volume charge density.
       }
     }
   }
