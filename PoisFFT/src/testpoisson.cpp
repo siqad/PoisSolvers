701b15bbbab4af87ebd87a7bc2c36c8024243156
NathanChiu <nathan.chiu94@gmail.com>
2018-01-23 16:20:57 -0800

    Output potential data to XML file too

--- PoisFFT/src/testpoisson.cpp
+++ PoisFFT/src/testpoisson.cpp
@@ -13,6 +13,7 @@
 #include <boost/foreach.hpp>
 #include "electrodes.hpp"
 #include <vector>
+#include <string>
 
 const double pi = 3.14159265358979323846;
 
@@ -20,7 +21,7 @@ const double pi = 3.14159265358979323846;
 #define SIMLENGTH 1.0e-6
 #define EPS0 8.85418782e-12
 #define Q_E 1.6e-19
-#define MAXERROR 1e-1
+#define MAXERROR 5e-2
 #define IND(i,j,k) (i)*(ns[1]*ns[2])+(j)*(ns[2])+k
 #define FILENAME ((char*)"outfile.txt")
 #define RHOFILE ((char*) "outrho.txt")
@@ -34,17 +35,21 @@ const double pi = 3.14159265358979323846;
 #define CHI_SI 4.05//electron affinity for silicon in eV from http://www.ioffe.ru/SVA/NSM/Semicond/Si/basic.html
 #define EPS_SI 11.7
 
-void save_file2D(const int[], const double[], const double[], double*, char[], std::string);
+
+void save_file2D(const int[], const double[], const double[], double*, char[], std::string, double, double, double);
+void save_fileXML(const int[], const double[], const double[], double*, char[], std::string, double, double, double, std::vector<Electrodes>);
+
 void check_solution(const int[], const double[], const double[], double*);
 void init_eps(const int[], const double[], const double[], double*);
 void init_rhs(const int[], const double[], const double[], double*, double*, double*);
 double check_error(const int[], const double[], double*, double*, std::pair<int,double>*, int*, double*, double*, const int[]);
 void apply_correction(const int[], const double[], double*, double*, std::pair<int,double>*);
 void create_electrode(const int[], const double[], const double[], double*, std::pair<int,double>*, double*, const int, Electrodes[]);
-void worker(int, std::vector<Electrodes>, std::string);
+
+void worker(int, std::vector<Electrodes>, std::string, double, double, double);
 void calc_charge(const int[], const double[], double*, const int, Electrodes[]);
 void parse_tree(std::vector<Electrodes>*, std::string);
-std::vector<Electrodes> set_buffer(std::vector<Electrodes>);
+std::vector<Electrodes> set_buffer(std::vector<Electrodes>, double*, double*, double*);
 
 int main(int argc,char* argv[]){
   int count = 0;
@@ -53,6 +58,10 @@ int main(int argc,char* argv[]){
   std::string arg1;
   std::string arg2;
   std::string outpath;
+  double finalscale = 0;
+  double xoffset = 0;
+  double yoffset = 0;
+
   for(int i = 0; i < argc; i++){
     std::cout << argv[i] << std::endl;
   }
@@ -64,27 +73,29 @@ int main(int argc,char* argv[]){
     if(arg1.find(".xml") != std::string::npos){ //argv[1] is an xml path, assume it is the INPUT file.
       std::cout << "Input path detected." << std::endl;
       parse_tree(&elec_vec, argv[1]);
-      elec_vec = set_buffer(elec_vec);
+      elec_vec = set_buffer(elec_vec, &finalscale, &xoffset, &yoffset);
+      std::cout << finalscale << " " << xoffset << " " << yoffset << std::endl;
       std::cout << "VECTOR SIZE " << elec_vec.size() << std::endl;
+      if(arg2.find(".xml") != std::string::npos){ //argv[2] is an xml path, assume it is the OUTPUT file.
+        std::cout << "Output path detected." << std::endl;
+        std::size_t found = arg2.find_last_of("/\\");
+        outpath = arg2.substr(0, found);
+        std::cout << outpath << std::endl;
+      }else{
+        std::cout << "Path not detected. Result XML path needs to be provided as second argument to binary. Terminating." << std::endl;
+      }
+      for(int i = 0; i < 1; i++){
+        worker(i, elec_vec, outpath, finalscale, xoffset, yoffset); //where the magic happens
+      }
+
     }else{
       std::cout << "Path not detected. Problem XML path needs to be provided as first argument to binary. Terminating." << std::endl;
-    }
-    if(arg2.find(".xml") != std::string::npos){ //argv[2] is an xml path, assume it is the OUTPUT file.
-      std::cout << "Output path detected." << std::endl;
-      std::size_t found = arg2.find_last_of("/\\");
-      outpath = arg2.substr(0, found);
-      std::cout << outpath << std::endl;
-      for(int i = 0; i < 1; i++){
-        worker(i, elec_vec, outpath); //where the magic happens
-      }
-    }else{
-      std::cout << "Path not detected. Result XML path needs to be provided as second argument to binary. Terminating." << std::endl;
     }
   }
   return 0;
 }
 
-std::vector<Electrodes> set_buffer(std::vector<Electrodes> elec_vec) {
+std::vector<Electrodes> set_buffer(std::vector<Electrodes> elec_vec, double* finalscale, double* xoffset, double* yoffset) {
   //want to scale electrodes down to fit the simulation space.
   //First, find the (min, max) (x, y) values.
   //Then, extend or contract so that 10% of the sim space is left on each edge as buffer space.
@@ -96,9 +107,9 @@ std::vector<Electrodes> set_buffer(std::vector<Electrodes> elec_vec) {
   double ylength;
   double xscale = 1;
   double yscale = 1;
-  double finalscale;
-  double xoffset = 0;
-  double yoffset = 0;
+  // double finalscale;
+  // double xoffset = 0;
+  // double yoffset = 0;
   // std::cout << "HELLO" << std::endl;
   for(int i = 0; i < elec_vec.size(); i++){
     xmin = std::min(xmin, elec_vec[i].x[0]);
@@ -107,31 +118,32 @@ std::vector<Electrodes> set_buffer(std::vector<Electrodes> elec_vec) {
     ymax = std::max(ymax, elec_vec[i].y[1]);
   }
 
-  // std::cout << "xmin: " << xmin << std::endl;
-  // std::cout << "xmax: " << xmax << std::endl;
-  // std::cout << "ymin: " << ymin << std::endl;
-  // std::cout << "ymax: " << ymax << std::endl;
+  std::cout << "xmin: " << xmin << std::endl;
+  std::cout << "xmax: " << xmax << std::endl;
+  std::cout << "ymin: " << ymin << std::endl;
+  std::cout << "ymax: " << ymax << std::endl;
 
   //x-scaling to keep 10% buffer on each horizontal side.
   if(xmin < 0.1*SIMLENGTH || xmax > 0.9*SIMLENGTH){
     xlength = xmax - xmin;
     xscale = 0.8*SIMLENGTH/xlength;
-    // std::cout << "xlength: " << xlength << std::endl;
+    std::cout << "xlength: " << xlength << std::endl;
   }
   if(ymin < 0.1*SIMLENGTH || ymax > 0.9*SIMLENGTH){
     ylength = ymax - ymin;
     yscale = 0.8*SIMLENGTH/ylength;
-    // std::cout << "ylength: " << ylength << std::endl;
+    std::cout << "ylength: " << ylength << std::endl;
   }
   // std::cout << "xscale: " << xscale << std::endl;
   // std::cout << "yscale: " << yscale << std::endl;
   //scale all elements by lowest scaling factor.
-  finalscale = std::min(xscale, yscale);
+  *finalscale = std::min(xscale, yscale);
+  std::cout << "Final scaling factor is: " << *finalscale << std::endl;
   for(int i = 0; i < elec_vec.size(); i++){
-    elec_vec[i].x[0] *= finalscale;
-    elec_vec[i].x[1] *= finalscale;
-    elec_vec[i].y[0] *= finalscale;
-    elec_vec[i].y[1] *= finalscale;
+    elec_vec[i].x[0] *= *(finalscale);
+    elec_vec[i].x[1] *= *(finalscale);
+    elec_vec[i].y[0] *= *(finalscale);
+    elec_vec[i].y[1] *= *(finalscale);
   }
   //now sample is sure to fit within simulation boundaries, with space for buffer.
   //translate the violating part to the buffer boundary, once for x and once for y.
@@ -139,34 +151,39 @@ std::vector<Electrodes> set_buffer(std::vector<Electrodes> elec_vec) {
   ymin = 100;
   xmax = 0;
   ymax = 0;
+  //find how far outside the boundary the shapes still sit.
   for(int i = 0; i < elec_vec.size(); i++){
     xmin = std::min(xmin, elec_vec[i].x[0]);
     ymin = std::min(ymin, elec_vec[i].y[0]);
     xmax = std::max(xmax, elec_vec[i].x[1]);
     ymax = std::max(ymax, elec_vec[i].y[1]);
   }
-  // std::cout << "xmin: " << xmin << std::endl;
-  // std::cout << "xmax: " << xmax << std::endl;
-  // std::cout << "ymin: " << ymin << std::endl;
-  // std::cout << "ymax: " << ymax << std::endl;
-  if(xmin < 0.1*SIMLENGTH){
+  std::cout << "xmin: " << xmin << std::endl;
+  std::cout << "xmax: " << xmax << std::endl;
+  std::cout << "ymin: " << ymin << std::endl;
+  std::cout << "ymax: " << ymax << std::endl;
+
+  if(xmin < 0.1*SIMLENGTH){  //too far to the left, find the offset
     //find the offset
-    xoffset = 0.1*SIMLENGTH - xmin;
-  }else if(xmax > 0.9*SIMLENGTH){
-    xoffset = xmax - 0.9*SIMLENGTH;
+    *xoffset = 0.1*SIMLENGTH - xmin;
+  }else if(xmax > 0.9*SIMLENGTH){ //too far right
+    *xoffset = xmax - 0.9*SIMLENGTH;
   }
-  if(ymin < 0.1*SIMLENGTH){
+  if(ymin < 0.1*SIMLENGTH){ //too far up
     //find the offset in y
-    yoffset = 0.1*SIMLENGTH - ymin;
-  }else if(ymax > 0.9*SIMLENGTH){
-    yoffset = ymax - 0.9*SIMLENGTH;
+    *yoffset = 0.1*SIMLENGTH - ymin;
+  }else if(ymax > 0.9*SIMLENGTH){ //too far down
+    *yoffset = ymax - 0.9*SIMLENGTH;
   }
+
+  std::cout << "X offset is: " << *xoffset << std::endl;
+  std::cout << "Y offset is: " << *yoffset << std::endl;
   //fix the offsets
-  for(int i = 0; i < elec_vec.size(); i++){
-    elec_vec[i].x[0] += xoffset;
-    elec_vec[i].x[1] += xoffset;
-    elec_vec[i].y[0] += yoffset;
-    elec_vec[i].y[1] += yoffset;
+  for(int i = 0; i < elec_vec.size(); i++){ //move all points based on offset.
+    elec_vec[i].x[0] += *(xoffset);
+    elec_vec[i].x[1] += *(xoffset);
+    elec_vec[i].y[0] += *(yoffset);
+    elec_vec[i].y[1] += *(yoffset);
     // std::cout << elec_vec[i].x[0] << " " << elec_vec[i].x[1] << ", " << elec_vec[i].y[0] << " " << elec_vec[i].y[1] << std::endl;
   }
   return elec_vec;
@@ -202,7 +219,7 @@ void parse_tree(std::vector<Electrodes> *elecs, std::string path){
                 potential = subtree2.get<double>(label);
                 std::cout << label << ":  " << potential << std::endl;
               }else if(label == "electrode_type"){ //electrode_type is the last one
-                elecs->push_back(Electrodes(pix_x1*1e-8, pix_x2*1e-8, pix_y1*1e-8, pix_y2*1e-8, 0.3e-6, 0.7e-6, potential, WF_GOLD));
+                elecs->push_back(Electrodes(pix_x1*SIMLENGTH, pix_x2*SIMLENGTH, pix_y1*SIMLENGTH, pix_y2*SIMLENGTH, 0.3e-6, 0.7e-6, potential, WF_GOLD));
               }else if(label != "<xmlattr>"){ //unexpected extras
                 std::string value = subtree2.get<std::string>(label);
                 std::cout << label << ":  " << value << std::endl;
@@ -216,7 +233,8 @@ void parse_tree(std::vector<Electrodes> *elecs, std::string path){
   std::cout << "Successfully read " << path << std::endl;
 }
 
-void worker(int step, std::vector<Electrodes> elec_vec, std::string resultpath){
+
+void worker(int step, std::vector<Electrodes> elec_vec, std::string resultpath, double finalscale, double xoffset, double yoffset){
   std::cout << "Modified by Nathan Chiu. Code is offered as is, with no warranty. See LICENCE.GPL for licence info." << std::endl;
   const double Ls[3] = {SIMLENGTH, SIMLENGTH, SIMLENGTH}; //x, y, z domain dimensions in MICROMETRE
   const int ns[3] = {SIMGRID, SIMGRID, SIMGRID}; //x, y, z gridpoint numbers
@@ -251,7 +269,7 @@ void worker(int step, std::vector<Electrodes> elec_vec, std::string resultpath){
   //   // Electrodes(0.6e-6, 0.8e-6, 0.2e-6, 0.4e-6, 0.3e-6, 0.7e-6, 1, WF_GOLD),
   //   // Electrodes(0.6e-6, 0.8e-6, 0.6e-6, 0.8e-6, 0.3e-6, 0.7e-6, 1, WF_GOLD)
   // };
-  elecs[step].potential = 2.0;
+  // elecs[step].potential = 2.0;
   double *arr = new double[ns[0]*ns[1]*ns[2]];
   double *arrOld = new double[ns[0]*ns[1]*ns[2]];
   double *eps = new double[ns[0]*ns[1]*ns[2]]; //RELATIVE permittivity.
@@ -277,9 +295,10 @@ void worker(int step, std::vector<Electrodes> elec_vec, std::string resultpath){
   calc_charge(ns, Ls, RHS, numElectrodes, elecs);
   // ProfilerStop();
   std::cout << "Finished on cycle " << cycleCount << " with error " << cycleErr << std::endl;
-  save_file2D(ns, ds, Ls, RHS, RHOFILE, resultpath);
-  save_file2D(ns, ds, Ls, arr, FILENAME, resultpath); //solution is in arr
-  save_file2D(ns, ds, Ls, correction, CORRECTIONFILE, resultpath);
+  save_file2D(ns, ds, Ls, RHS, RHOFILE, resultpath, finalscale, xoffset, yoffset);
+  save_file2D(ns, ds, Ls, arr, FILENAME, resultpath, finalscale, xoffset, yoffset); //solution is in arr
+  save_file2D(ns, ds, Ls, correction, CORRECTIONFILE, resultpath, finalscale, xoffset, yoffset);
+  save_fileXML(ns, ds, Ls, arr, "XMLOUT", resultpath, finalscale, xoffset, yoffset, elec_vec);
   std::cout << "Time elapsed: " << float(clock()-begin_time)/CLOCKS_PER_SEC << " seconds" << std::endl;
   std::cout << "Ending, deleting variables" << std::endl;
   delete indexErr;
@@ -358,7 +377,8 @@ double check_error(const int ns[3], const double Ls[3], double *arr, double *cor
   return err;
 }
 
-void save_file2D(const int ns[3], const double ds[3], const double Ls[3], double* arr, char fname[], std::string pathname){
+
+void save_file2D(const int ns[3], const double ds[3], const double Ls[3], double* arr, char fname[], std::string pathname, double finalscale, double xoffset, double yoffset){
   // std::string finalpath = fname;
   std::string temp = fname;
   std::string finalpath = pathname+"/"+temp;
@@ -371,11 +391,91 @@ void save_file2D(const int ns[3], const double ds[3], const double Ls[3], double
   const int k = ns[2]/2;
   for (int i = 0; i < ns[0]; i++){
     for (int j = 0; j < ns[1]; j++){
-        outfile << std::setprecision(5) << std::scientific << i*ds[0] << " " << j*ds[1] << " " << arr[IND(i,j,k)] << std::endl;
+      outfile << std::setprecision(5) << std::scientific << (i*ds[0]-xoffset)/finalscale << " "
+              << (j*ds[1]-yoffset)/finalscale << " " << arr[IND(i,j,k)] << std::endl;
     }
     outfile << std::endl;
   }
 }
+
+
+void save_fileXML(const int ns[3], const double ds[3], const double Ls[3], double* arr, char fname[], std::string pathname, double finalscale, double xoffset, double yoffset, std::vector<Electrodes> elec_vec){
+
+  // define major XML nodes
+  boost::property_tree::ptree tree;
+  boost::property_tree::ptree node_root;       // <sim_out>
+  boost::property_tree::ptree node_eng_info;   // <eng_info>
+  boost::property_tree::ptree node_sim_params; // <sim_params>
+  boost::property_tree::ptree node_electrode;  // <electrode>
+  boost::property_tree::ptree node_potential_map;  // <potential>
+
+  // std::string finalpath = fname;
+  std::string temp = fname;
+  std::string finalpath = pathname+"/"+temp;
+
+  std::cout << finalpath << std::endl;
+  std::cout << "Write results to XML..." << std::endl;
+  // NOTE in the future, there's probably a range of stuff that can be exported.
+  // for now, only export charge config
+
+  // eng_info
+  node_eng_info.put("engine", "PoisSolver");
+  node_eng_info.put("version", "TBD"); // TODO real version
+
+  // sim_params
+  // TODO
+
+  // electrode
+  for (auto elec : elec_vec) {
+    boost::property_tree::ptree node_dim;
+    node_dim.put("<xmlattr>.x1", (std::to_string((elec.x[0]-xoffset)/finalscale/SIMLENGTH).c_str()));
+    node_dim.put("<xmlattr>.y1", (std::to_string((elec.y[0]-yoffset)/finalscale/SIMLENGTH).c_str()));
+    node_dim.put("<xmlattr>.x2", (std::to_string((elec.x[1]-xoffset)/finalscale/SIMLENGTH).c_str()));
+    node_dim.put("<xmlattr>.y2", (std::to_string((elec.y[1]-yoffset)/finalscale/SIMLENGTH).c_str()));
+    node_electrode.add_child("dim", node_dim);
+    boost::property_tree::ptree node_pot;
+    node_pot.put("", std::to_string(elec.potential).c_str());
+    node_electrode.add_child("potential", node_pot);
+  }
+
+  //potential_map
+  const int k = ns[2]/2;
+  for (int i = 0; i < ns[0]; i++){
+    for (int j = 0; j < ns[1]; j++){
+      //create each entry
+      boost::property_tree::ptree node_potential_val;
+      node_potential_val.put("<xmlattr>.x", (std::to_string((i*ds[0]-xoffset)/finalscale/SIMLENGTH)).c_str());
+      node_potential_val.put("<xmlattr>.y", (std::to_string((j*ds[1]-yoffset)/finalscale/SIMLENGTH)).c_str());
+      node_potential_val.put("<xmlattr>.val", std::to_string(arr[IND(i,j,k)]).c_str());
+      node_potential_map.add_child("potential_val", node_potential_val);
+    }
+  }
+
+  // // elec_dist
+  // for (auto db_charge : db_charges) {
+  //   boost::property_tree::ptree node_dist;
+  //   std::string dbc_link;
+  //   for(auto chg : db_charge)
+  //     dbc_link.append(std::to_string(chg));
+  //   node_dist.put("", dbc_link);
+  //   node_elec_dist.add_child("dist", node_dist);
+  // }
+  //
+  // // add nodes to appropriate parent
+  node_root.add_child("eng_info", node_eng_info);
+  // node_root.add_child("sim_params", node_sim_params);
+  node_root.add_child("electrode", node_electrode);
+  node_root.add_child("potential_map", node_potential_map);
+  // node_root.add_child("elec_dist", node_elec_dist);
+  tree.add_child("sim_out", node_root);
+
+  // write to file
+  boost::property_tree::write_xml(finalpath, tree, std::locale(), boost::property_tree::xml_writer_make_settings<std::string>(' ',4));
+
+  std::cout << "Write to XML complete." << std::endl;
+
+}
+
 
 void init_eps(const int ns[3], const double ds[3], const double Ls[3], double* a){
   int i,j,k;
