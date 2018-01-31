d676d1c9a19cfdc5cb186439e273c8d00742ca24
NathanChiu <nathan.chiu94@gmail.com>
2018-01-31 12:51:43 -0800

    Remove simulation parameters from functions defined in poissolver.cpp. Use instead SimParams namespace

--- PoisFFT/src/poissolver.cpp
+++ PoisFFT/src/poissolver.cpp
@@ -17,7 +17,6 @@
 #include <string>
 
 // Physical constants are placed inside PhysConstants namespace.
-
 // #define SIMGRID 50
 // #define SIMLENGTH 1.0e-6
 // #define MAX_ERROR 5e-2
@@ -29,22 +28,19 @@
 
 
 void save_file2D(double* arr, char fname[], std::string pathname, double finalscale, double xoffset, double yoffset);
-void save_fileXML(const int[], const double[], const double[], double*, char[], std::string, double, double, double, std::vector<Electrodes>);
-
-void check_solution(const int[], const double[], const double[], double*);
-void init_eps(const int[], const double[], const double[], double*);
-void init_rhs(const int[], const double[], const double[], double*, double*, double*);
-double check_error(const int[], const double[], double*, double*, std::pair<int,double>*, int*, double*, double*, const int[]);
-void apply_correction(const int[], const double[], double*, double*, std::pair<int,double>*);
-void create_electrode(const int[], const double[], const double[], double*, std::pair<int,double>*, double*, const int, Electrodes[]);
-
+void save_fileXML(double* arr, char fname[], std::string pathname, double finalscale, double xoffset, double yoffset, std::vector<Electrodes> elec_vec);
+void init_eps(double* eps);
+void init_rhs(double* chi, double* eps, double* rhs);
+double check_error(double *arr, double *correction, std::pair<int,double> *electrodemap, int *indexErr, double *eps);
+void apply_correction(double *RHS, double *correction, std::pair<int,double> *electrodemap);
+void create_electrode(double* RHS, std::pair<int,double> *electrodemap, double* chi, const int numElectrodes, Electrodes elecs[]);
 void worker(int, std::vector<Electrodes>, std::string, double, double, double);
-void calc_charge(const int[], const double[], double*, const int, Electrodes[]);
+void calc_charge(double* RHS , const int numElectrodes, Electrodes elecs[]);
 void parse_tree(std::vector<Electrodes>*, std::string);
 std::vector<Electrodes> set_buffer(std::vector<Electrodes>, double*, double*, double*);
 
-int main(int argc,char* argv[]){
 
+int main(int argc,char* argv[]){
   std::cout << "Number of command line arguments: " << argc << std::endl;
   std::vector<Electrodes> elec_vec;
   std::string arg1;
@@ -53,13 +49,6 @@ int main(int argc,char* argv[]){
   double finalscale = 0;
   double xoffset = 0;
   double yoffset = 0;
-
-  for(int i = 0; i < argc; i++){
-    std::cout << argv[i] << std::endl;
-  }
-
-  std::cout << SimParams::Ls[0] << " " << SimParams::Ls[1] << " " << SimParams::Ls[2] << std::endl;
-  std::cout << SimParams::ns[0] << " " << SimParams::ns[1] << " " << SimParams::ns[2] << std::endl;
 
   if(argc == 1){
     std::cout << "No path was passed to the solver program. Program terminating." << std::endl;
@@ -77,13 +66,12 @@ int main(int argc,char* argv[]){
         std::size_t found = arg2.find_last_of("/\\");
         outpath = arg2.substr(0, found);
         std::cout << outpath << std::endl;
+        for(int i = 0; i < 1; i++){
+          worker(i, elec_vec, outpath, finalscale, xoffset, yoffset); //where the magic happens
+        }
       }else{
         std::cout << "Path not detected. Result XML path needs to be provided as second argument to binary. Terminating." << std::endl;
       }
-      for(int i = 0; i < 1; i++){
-        worker(i, elec_vec, outpath, finalscale, xoffset, yoffset); //where the magic happens
-      }
-
     }else{
       std::cout << "Path not detected. Problem XML path needs to be provided as first argument to binary. Terminating." << std::endl;
     }
@@ -104,9 +92,6 @@ std::vector<Electrodes> set_buffer(std::vector<Electrodes> elec_vec, double* fin
   double ylength;
   double xscale = 1;
   double yscale = 1;
-  // double finalscale;
-  // double xoffset = 0;
-  // double yoffset = 0;
   for(int i = 0; i < elec_vec.size(); i++){
     xmin = std::min(xmin, elec_vec[i].x[0]);
     ymin = std::min(ymin, elec_vec[i].y[0]);
@@ -130,8 +115,6 @@ std::vector<Electrodes> set_buffer(std::vector<Electrodes> elec_vec, double* fin
     yscale = 0.8*SimParams::Ls[1]/ylength;
     std::cout << "ylength: " << ylength << std::endl;
   }
-  // std::cout << "xscale: " << xscale << std::endl;
-  // std::cout << "yscale: " << yscale << std::endl;
   //scale all elements by lowest scaling factor.
   *finalscale = std::min(xscale, yscale);
   std::cout << "Final scaling factor is: " << *finalscale << std::endl;
@@ -278,7 +261,6 @@ void worker(int step, std::vector<Electrodes> elec_vec, std::string resultpath, 
 
   const int nsize = SimParams::ns[0]*SimParams::ns[1]*SimParams::ns[2]; //array size
   double *arr = new double[nsize];
-  double *arrOld = new double[nsize];
   double *eps = new double[nsize]; //RELATIVE permittivity.
   double *chi = new double[nsize]; //electron affinity or WF
   double *RHS = new double[nsize]; // from which you can get a pointer to contiguous buffer, will contain rho.
@@ -286,32 +268,27 @@ void worker(int step, std::vector<Electrodes> elec_vec, std::string resultpath, 
   std::pair<int,double> *electrodemap = new std::pair<int,double>[nsize]; //stores electrode surface info and potentials.
   int cycleCount = 0;
   const std::clock_t begin_time = std::clock();
-  init_eps(SimParams::ns, SimParams::ds, SimParams::Ls, eps); // set permittivity
-  init_rhs(SimParams::ns, SimParams::ds, SimParams::Ls, chi, eps, RHS); // set rho and apply relative permittivity, also save electron affinity for bulk.
-  create_electrode(SimParams::ns, SimParams::ds, SimParams::Ls, RHS, electrodemap, chi, numElectrodes, elecs);
+  init_eps(eps); // set permittivity
+  init_rhs(chi, eps, RHS); // set rho and apply relative permittivity, also save electron affinity for bulk.
+  create_electrode(RHS, electrodemap, chi, numElectrodes, elecs);
   PoisFFT::Solver<3, double> S(SimParams::ns, SimParams::Ls, SimParams::BCs); //   create solver object, 3 dimensions, double precision
-
 
   std::cout << "Beginning solver" << std::endl;
   do{
     S.execute(arr, RHS); //run the solver, can be run many times for different right-hand side
-    cycleErr = check_error(SimParams::ns, SimParams::Ls, arr, correction, electrodemap, indexErr, arrOld, eps, SimParams::BCs);
-    apply_correction(SimParams::ns, SimParams::ds, RHS, correction, electrodemap);
+    cycleErr = check_error(arr, correction, electrodemap, indexErr, eps);
+    apply_correction(RHS, correction, electrodemap);
     std::cout << "On cycle " << cycleCount << " with error " << cycleErr << " at index " << *indexErr << ". " << arr[*indexErr] << " " << electrodemap[*indexErr].second << std::endl;
     cycleCount++;
   }while(cycleErr > SimParams::MAX_ERROR && cycleErr != 0);
 
-
-  calc_charge(SimParams::ns, SimParams::Ls, RHS, numElectrodes, elecs);
+  calc_charge(RHS, numElectrodes, elecs);
   std::cout << "Finished on cycle " << cycleCount << " with error " << cycleErr << std::endl;
-
-
   save_file2D(RHS, RHOFILE, resultpath, finalscale, xoffset, yoffset);
   save_file2D(arr, FILENAME, resultpath, finalscale, xoffset, yoffset); //solution is in arr
   save_file2D(correction, CORRECTIONFILE, resultpath, finalscale, xoffset, yoffset);
   // use char* to prevent compiler warning text
-  save_fileXML(SimParams::ns, SimParams::ds, SimParams::Ls, arr, (char*)"sim_result.xml", resultpath, finalscale, xoffset, yoffset, elec_vec);
-
+  save_fileXML(arr, (char*)"sim_result.xml", resultpath, finalscale, xoffset, yoffset, elec_vec);
 
   std::cout << "Time elapsed: " << float(clock()-begin_time)/CLOCKS_PER_SEC << " seconds" << std::endl;
   std::cout << "Ending, deleting variables" << std::endl;
@@ -326,7 +303,7 @@ void worker(int step, std::vector<Electrodes> elec_vec, std::string resultpath, 
   delete[] correction;
 }
 
-void calc_charge( const int ns[3], const double Ls[3], double* RHS , const int numElectrodes, Electrodes elecs[]){
+void calc_charge(double* RHS , const int numElectrodes, Electrodes elecs[]){
   //Want this to take in electrode location parameters, and spit out the charge on the conductor.
   double xmin, xmax, ymin, ymax, zmin, zmax, sum;
   for( int currElectrode = 0; currElectrode < numElectrodes; currElectrode++){
@@ -337,9 +314,9 @@ void calc_charge( const int ns[3], const double Ls[3], double* RHS , const int n
     zmin = elecs[currElectrode].z[0];
     zmax = elecs[currElectrode].z[1];
     sum = 0;
-    for(int i = (int) ns[0]*xmin/Ls[0]; i <= (int) ns[0]*xmax/Ls[0]; i++){ //xmin first, then xmax
-      for(int j = (int) ns[1]*ymin/Ls[1]; j <= (int) ns[1]*ymax/Ls[1]; j++){
-        for(int k = (int) ns[2]*zmin/Ls[2]; k <= (int) ns[2]*zmax/Ls[2]; k++){
+    for(int i = (int) SimParams::ns[0]*xmin/SimParams::Ls[0]; i <= (int) SimParams::ns[0]*xmax/SimParams::Ls[0]; i++){ //xmin first, then xmax
+      for(int j = (int) SimParams::ns[1]*ymin/SimParams::Ls[1]; j <= (int) SimParams::ns[1]*ymax/SimParams::Ls[1]; j++){
+        for(int k = (int) SimParams::ns[2]*zmin/SimParams::Ls[2]; k <= (int) SimParams::ns[2]*zmax/SimParams::Ls[2]; k++){
           sum += RHS[SimParams::IND(i,j,k)];
         }
       }
@@ -349,32 +326,30 @@ void calc_charge( const int ns[3], const double Ls[3], double* RHS , const int n
   }
 }
 
-void create_electrode(const int ns[3], const double ds[3], const double Ls[3], double* RHS, std::pair<int,double> *electrodemap, double* chi, const int numElectrodes, Electrodes elecs[]){
+void create_electrode(double* RHS, std::pair<int,double> *electrodemap, double* chi, const int numElectrodes, Electrodes elecs[]){
   for(int i = 0; i < numElectrodes; i++){
-    elecs[i].draw(ns, ds, Ls, RHS, electrodemap, chi); //separately call draw for each electrode.
+    elecs[i].draw(SimParams::ns, SimParams::ds, SimParams::Ls, RHS, electrodemap, chi); //separately call draw for each electrode.
   }
 }
 
-void apply_correction(const int ns[3], const double ds[3], double *RHS, double *correction, std::pair<int,double> *electrodemap){
-  for(int i = 0; i < ns[0]*ns[1]*ns[2]; i++){
+void apply_correction(double *RHS, double *correction, std::pair<int,double> *electrodemap){
+  for(int i = 0; i < SimParams::ns[0]*SimParams::ns[1]*SimParams::ns[2]; i++){
     if(electrodemap[i].first == true){ //only correct error at electrode surfaces.
       RHS[i] -= correction[i];
     }
   }
 }
 
-double check_error(const int ns[3], const double Ls[3], double *arr, double *correction, std::pair<int,double> *electrodemap, int *indexErr,
-                   double *arrOld, double *eps, const int BCs[6]){
+double check_error(double *arr, double *correction, std::pair<int,double> *electrodemap, int *indexErr, double *eps){
   double err = 0;
   double errOld;
   double correctionWeight;
-  if(BCs[0] == PoisFFT::PERIODIC){
-    correctionWeight = 1e-7*ns[0]*PhysConstants::EPS0/PhysConstants::QE/Ls[0]/Ls[0]; //Periodic
-  } else if(BCs[0] == PoisFFT::DIRICHLET || BCs[0] == PoisFFT::NEUMANN){
-    correctionWeight = 0.5e-7*ns[0]*PhysConstants::EPS0/PhysConstants::QE/Ls[0]/Ls[0];  //Dirichlet & Neumann
+  if(SimParams::BCs[0] == PoisFFT::PERIODIC){
+    correctionWeight = 1e-7*SimParams::ns[0]*PhysConstants::EPS0/PhysConstants::QE/SimParams::Ls[0]/SimParams::Ls[0]; //Periodic
+  } else if(SimParams::BCs[0] == PoisFFT::DIRICHLET || SimParams::BCs[0] == PoisFFT::NEUMANN){
+    correctionWeight = 0.5e-7*SimParams::ns[0]*PhysConstants::EPS0/PhysConstants::QE/SimParams::Ls[0]/SimParams::Ls[0];  //Dirichlet & Neumann
   }
-  // for(int i = 0; i < ns[0]*ns[1]*ns[2]; i++){
-  for(int i = 0; i < ns[0]*ns[1]*ns[2]; i++){
+  for(int i = 0; i < SimParams::ns[0]*SimParams::ns[1]*SimParams::ns[2]; i++){
     if(electrodemap[i].first == true){ //only check error at electrodes.
       errOld = err;
       correction[i] = electrodemap[i].second-arr[i]; //intended potential - found potential. Looking at laplacian of potential doesn't allow snapping to electrode potentials.
@@ -388,7 +363,6 @@ double check_error(const int ns[3], const double Ls[3], double *arr, double *cor
         *indexErr = i;
       }
     }
-    // arrOld[i] = arr[i];
   }
   return err;
 }
@@ -414,7 +388,7 @@ void save_file2D(double* arr, char fname[], std::string pathname, double finalsc
 }
 
 
-void save_fileXML(const int ns[3], const double ds[3], const double Ls[3], double* arr, char fname[], std::string pathname, double finalscale, double xoffset, double yoffset, std::vector<Electrodes> elec_vec){
+void save_fileXML(double* arr, char fname[], std::string pathname, double finalscale, double xoffset, double yoffset, std::vector<Electrodes> elec_vec){
 
   // define major XML nodes
   boost::property_tree::ptree tree;
@@ -454,22 +428,20 @@ void save_fileXML(const int ns[3], const double ds[3], const double Ls[3], doubl
   }
 
   //potential_map
-  const int k = ns[2]/2;
-  for (int i = 0; i < ns[0]; i++){
-    for (int j = 0; j < ns[1]; j++){
+  const int k = SimParams::ns[2]/2;
+  for (int i = 0; i < SimParams::ns[0]; i++){
+    for (int j = 0; j < SimParams::ns[1]; j++){
       //create each entry
       boost::property_tree::ptree node_potential_val;
-      node_potential_val.put("<xmlattr>.x", (std::to_string((i*ds[0]-xoffset)/finalscale/SimParams::Ls[0])).c_str());
-      node_potential_val.put("<xmlattr>.y", (std::to_string((j*ds[1]-yoffset)/finalscale/SimParams::Ls[1])).c_str());
+      node_potential_val.put("<xmlattr>.x", (std::to_string((i*SimParams::ds[0]-xoffset)/finalscale/SimParams::Ls[0])).c_str());
+      node_potential_val.put("<xmlattr>.y", (std::to_string((j*SimParams::ds[1]-yoffset)/finalscale/SimParams::Ls[1])).c_str());
       node_potential_val.put("<xmlattr>.val", std::to_string(arr[SimParams::IND(i,j,k)]).c_str());
       node_potential_map.add_child("potential_val", node_potential_val);
     }
   }
   node_root.add_child("eng_info", node_eng_info);
-  // node_root.add_child("sim_params", node_sim_params);
   node_root.add_child("electrode", node_electrode);
   node_root.add_child("potential_map", node_potential_map);
-  // node_root.add_child("elec_dist", node_elec_dist);
   tree.add_child("sim_out", node_root);
 
   // write to file
@@ -480,21 +452,20 @@ void save_fileXML(const int ns[3], const double ds[3], const double Ls[3], doubl
 }
 
 
-void init_eps(const int ns[3], const double ds[3], const double Ls[3], double* a){
+void init_eps(double* eps){
   int i,j,k;
   std::cout << "Initialising eps" << std::endl;
-  for (i=0;i<ns[0];i++){
-    double x = ds[0]*(i+0.5);
-    for (j=0;j<ns[1];j++){
-      double y = ds[1]*(j+0.5);
-      for (k=0;k<ns[2];k++){
-        double z = ds[2]*(k+0.5);
-        if (y < Ls[1]/2){
+  for (i=0;i<SimParams::ns[0];i++){
+    double x = SimParams::ds[0]*(i+0.5);
+    for (j=0;j<SimParams::ns[1];j++){
+      double y = SimParams::ds[1]*(j+0.5);
+      for (k=0;k<SimParams::ns[2];k++){
+        double z = SimParams::ds[2]*(k+0.5);
+        if (y < SimParams::Ls[1]/2){
           // a[IND(i,j,k)] = PhysConstants::EPS_SI; //Si relative permittivity
-          a[SimParams::IND(i,j,k)] = 1;  //Free space
+          eps[SimParams::IND(i,j,k)] = 1;  //Free space
         } else{
-          // a[IND(i,j,k)] = PhysConstants::EPS_SI; //Si relative permittivity
-          a[SimParams::IND(i,j,k)] = 1;  //Free space
+          eps[SimParams::IND(i,j,k)] = 1;  //Free space
         }
       }
     }
@@ -502,17 +473,17 @@ void init_eps(const int ns[3], const double ds[3], const double Ls[3], double* a
   std::cout << "Finished rho initialisation" << std::endl;
 }
 
-void init_rhs(const int ns[3], const double ds[3], const double Ls[3], double* chi, double* eps, double* a){
+void init_rhs(double* chi, double* eps, double* rhs){
   int i,j,k;
   std::cout << "Initialising RHS" << std::endl;
-  for (i=0;i<ns[0];i++){
-    double x = ds[0]*(i+0.5);
-    for (j=0;j<ns[1];j++){
-      double y = ds[1]*(j+0.5);
-      for (k=0;k<ns[2];k++){
-        double z = ds[2]*(k+0.5);
+  for (i=0;i<SimParams::ns[0];i++){
+    double x = SimParams::ds[0]*(i+0.5);
+    for (j=0;j<SimParams::ns[1];j++){
+      double y = SimParams::ds[1]*(j+0.5);
+      for (k=0;k<SimParams::ns[2];k++){
+        double z = SimParams::ds[2]*(k+0.5);
         // a[IND(i,j,k)] = 1e16*PhysConstants::QE/PhysConstants::EPS0/eps[IND(i,j,k)]; //in m^-3, scale by permittivity
-        a[SimParams::IND(i,j,k)] = 0; //in m^-3, scale by permittivity
+        rhs[SimParams::IND(i,j,k)] = 0; //in m^-3, scale by permittivity
         chi[SimParams::IND(i,j,k)] = PhysConstants::CHI_SI;
       }
     }
