892cf26a88132eefe05a50f427564caec6c57b6c
NathanChiu <nathan.chiu94@gmail.com>
2018-02-02 13:39:09 -0800

    Copy over problem and phys_eng files from SimAnneal, split poissolver.cpp into poismain.cpp and poissolver.cpp. Move all non-class essential stuff to poismain.cpp. Namespace declared in poissolver.h, but the simulation parameters are defined in poismain, and the physics constants are defined in poissolver.cpp

--- PoisFFT/src/poissolver.h
+++ PoisFFT/src/poissolver.h
@@ -21,40 +21,38 @@
 // Physical constants used for calculation
 namespace PhysConstants
 {
-   const double QE = 1.6021766208e-19; //elementary charge
-   const double EPS0 = 8.85418782e-12; //permittivity of free space
-   const double WF_GOLD = 5.1; //workfunction for gold in eV
-   const double WF_COPPER = 4.7; //workfunction for copper in eV
-   const double WF_ZINC = 4.3; //workfunction for zinc in eV
-   const double WF_CESIUM = 2.1; //workfunction for cesium in eV
-   const double WF_NICKEL = 5.01; //workfunction for nickel in eV
-   const double CHI_SI = 4.05; //electron affinity for silicon in eV from http://www.ioffe.ru/SVA/NSM/Semicond/Si/basic.html
-   const double EPS_SI = 11.7; //relative permittivity of silicon
+   extern const double QE; //elementary charge
+   extern const double EPS0; //permittivity of free space
+   extern const double WF_GOLD; //workfunction for gold in eV
+   extern const double WF_COPPER; //workfunction for copper in eV
+   extern const double WF_ZINC; //workfunction for zinc in eV
+   extern const double WF_CESIUM; //workfunction for cesium in eV
+   extern const double WF_NICKEL; //workfunction for nickel in eV
+   extern const double CHI_SI; //electron affinity for silicon in eV from http://www.ioffe.ru/SVA/NSM/Semicond/Si/basic.html
+   extern const double EPS_SI; //relative permittivity of silicon
 };
 
 // Simulation parameters used for simulation control
 namespace SimParams
 {
   // scaling and offset values
-  double finalscale, xoffset, yoffset;
-  std::string resultpath;
+  extern double finalscale, xoffset, yoffset;
+  extern std::string resultpath;
 
   //stuff used during simulation
-  double Ls[3] = {1e-6, 1e-6, 1e-6}; // simulation length in x, y, z
-  int ns[3] = {50, 50, 50}; // resolution in x, y, z
-  double ds[3]; // simulation length per resolution step, CALCULATED
-  int BCs[6]  = {PoisFFT::NEUMANN, PoisFFT::NEUMANN,
-                 PoisFFT::NEUMANN, PoisFFT::NEUMANN,
-                 PoisFFT::NEUMANN, PoisFFT::NEUMANN}; // boundary conditions for left, right, top, bottom, front, back.
-  double MAX_ERROR = 5e-2;
-  int IND(int i, int j, int k){ return (i)*(ns[1]*ns[2]) + (j)*(ns[2]) + k; };
+  extern double Ls[3]; // simulation length in x, y, z
+  extern int ns[3]; // resolution in x, y, z
+  extern double ds[3]; // simulation length per resolution step, CALCULATED
+  extern int BCs[6]; // boundary conditions for left, right, top, bottom, front, back.
+  extern double MAX_ERROR;
+  extern int IND(int i, int j, int k);
 
   //stuff used post-simulation
-  char* OUTFILE = (char*) "outfile.txt";
-  char* RHOFILE = (char*) "rhofile.txt";
-  char* EPSFILE = (char*) "epsfile.txt";
-  char* CORRFILE = (char*) "corrfile.txt";
-  char* RESXML = (char*) "sim_result.xml";
+  extern char* OUTFILE;
+  extern char* RHOFILE;
+  extern char* EPSFILE;
+  extern char* CORRFILE;
+  extern char* RESXML;
 };
 
 class PoisSolver
