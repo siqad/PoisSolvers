5d887384f805de4c22759cbe7471fcfc16c14fb7
NathanChiu <nathan.chiu94@gmail.com>
2018-03-13 11:02:03 -0700

    Add problem features into physics connector

--- PoisFFT/src/phys_connector.cpp
+++ PoisFFT/src/phys_connector.cpp
@@ -21,10 +21,279 @@ PhysicsConnector::PhysicsConnector(const std::string &eng_name_in,
   eng_name = eng_name_in;
   input_path = input_path_in;
   output_path = output_path_in;
+  initProblem();
+  readProblem(input_path);
 }
 
 
+//What used to be problem
+
+void PhysicsConnector::initProblem(void)
+{
+  elec_tree = std::make_shared<PhysicsConnector::Aggregate>();
+}
+
+// aggregate
+int PhysicsConnector::Aggregate::size()
+{
+  int n_elecs=elecs.size();
+  if(!aggs.empty())
+    for(auto agg : aggs)
+      n_elecs += agg->size();
+  return n_elecs;
+}
+
+// ELEC ITERATOR
+PhysicsConnector::ElecIterator::ElecIterator(std::shared_ptr<Aggregate> root, bool begin)
+{
+  if(begin){
+    // keep finding deeper aggregates until one that contains dbs is found
+    while(root->elecs.empty() && !root->aggs.empty()) {
+      push(root);
+      root = root->aggs.front();
+    }
+    push(root);
+  }
+  else{
+    elec_iter = root->elecs.cend();
+  }
+}
+
+PhysicsConnector::ElecIterator& PhysicsConnector::ElecIterator::operator++()
+{
+  // exhaust the current Aggregate DBs first
+  if(elec_iter != curr->elecs.cend())
+    return ++elec_iter != curr->elecs.cend() ? *this : ++(*this);
+
+  // if available, push the next aggregate onto the stack
+  if(agg_stack.top().second != curr->aggs.cend()){
+    push(*agg_stack.top().second);
+    return elec_iter != curr->elecs.cend() ? *this : ++(*this);
+  }
+
+  // aggregate is complete, pop off stack
+  pop();
+  return agg_stack.size() == 0 ? *this : ++(*this);
+}
+
+void PhysicsConnector::ElecIterator::push(std::shared_ptr<Aggregate> agg)
+{
+  if(!agg_stack.empty())
+    ++agg_stack.top().second;
+  agg_stack.push(std::make_pair(agg, agg->aggs.cbegin()));
+  elec_iter = agg->elecs.cbegin();
+  curr = agg;
+}
+
+void PhysicsConnector::ElecIterator::pop()
+{
+  agg_stack.pop();              // pop complete aggregate off stack
+  if(agg_stack.size() > 0){
+    curr = agg_stack.top().first; // update current to new top
+    elec_iter = curr->elecs.cend();   // don't reread dbs
+  }
+}
+
+// FILE HANDLING
+// parse problem XML, return true if successful
+bool PhysicsConnector::readProblem(const std::string &fname)
+{
+  std::cout << "Reading problem file: " << fname << std::endl;
+
+  bpt::ptree tree; // create empty property tree object
+  bpt::read_xml(fname, tree, bpt::xml_parser::no_comments); // parse the input file into property tree
+  // TODO catch read error exception
+
+  // parse XML
+
+  // read program properties
+  // TODO read program node
+
+  // read material properties
+  // TODO read material_prop node
+
+  // read simulation parameters
+  std::cout << "Read simulation parameters" << std::endl;
+  if(!readSimulationParam(tree.get_child("dbdesigner.sim_params")))
+    return false;
+
+  // read items
+  std::cout << "Read items tree" << std::endl;
+  if(!readDesign(tree.get_child("dbdesigner.design"), elec_tree))
+    return false;
+
+  //return true;
+  return false;
+}
+
+bool PhysicsConnector::readProgramProp(const bpt::ptree &program_prop_tree)
+{
+  for (bpt::ptree::value_type const &v : program_prop_tree) {
+    program_props.insert(std::map<std::string, std::string>::value_type(v.first, v.second.data()));
+    std::cout << "ProgramProp: Key=" << v.first << ", Value=" << program_props[v.first] << std::endl;
+  }
+  return true;
+}
+
+bool PhysicsConnector::readMaterialProp(const bpt::ptree &material_prop_tree)
+{
+  (void)material_prop_tree; // function to be implemented, suppress variable unused warning for now
+  return true;
+}
+
+bool PhysicsConnector::readSimulationParam(const bpt::ptree &sim_params_tree)
+{
+  for (bpt::ptree::value_type const &v : sim_params_tree) {
+    sim_params.insert(std::map<std::string, std::string>::value_type(v.first, v.second.data()));
+    std::cout << "SimParam: Key=" << v.first << ", Value=" << sim_params[v.first] << std::endl;
+  }
+  return true;
+}
+
+bool PhysicsConnector::readDesign(const bpt::ptree &subtree, const std::shared_ptr<Aggregate> &agg_parent)
+{
+  std::cout << "Beginning to read design" << std::endl;
+  for (bpt::ptree::value_type const &layer_tree : subtree) {
+    std::string layer_type = layer_tree.second.get<std::string>("<xmlattr>.type");
+    if (!layer_type.compare("DB")) {
+      std::cout << "Encountered node " << layer_tree.first << " with type " << layer_type << ", entering" << std::endl;
+      readItemTree(layer_tree.second, agg_parent);
+    } else if (!layer_type.compare("Electrode")) {
+    // if (!layer_type.compare("Electrode")) {
+      // std::cout << "TODO write code for parsing electrodes" << std::endl;
+      std::cout << "Encountered node " << layer_tree.first << " with type " << layer_type << ", entering" << std::endl;
+      readItemTree(layer_tree.second, agg_parent);
+    } else {
+      std::cout << "Encountered node " << layer_tree.first << " with type " << layer_type << ", no defined action for this layer. Skipping." << std::endl;
+    }
+  }
+  return true;
+}
+
+bool PhysicsConnector::readItemTree(const bpt::ptree &subtree, const std::shared_ptr<Aggregate> &agg_parent)
+{
+  for (bpt::ptree::value_type const &item_tree : subtree) {
+    std::string item_name = item_tree.first;
+
+    if (!item_name.compare("aggregate")) {
+      // add aggregate child to tree
+      agg_parent->aggs.push_back(std::make_shared<Aggregate>());
+      readItemTree(item_tree.second, agg_parent->aggs.back());
+    // } else if (!item_name.compare("dbdot")) {
+    //   // add DBDot to tree
+    //   readDBDot(item_tree.second, agg_parent);
+    } else if (!item_name.compare("electrode")) {
+      // add Electrode to tree
+      readElectrode(item_tree.second, agg_parent);
+    } else {
+      std::cout << "Encountered unknown item node: " << item_tree.first << std::endl;
+    }
+  }
+  return true;
+}
+
+bool PhysicsConnector::readElectrode(const bpt::ptree &subtree, const std::shared_ptr<Aggregate> &agg_parent)
+{
+  float x1, x2, y1, y2;
+  float potential;
+
+  // read values from XML stream
+  potential = subtree.get<float>("potential");
+  x1 = subtree.get<float>("dim.<xmlattr>.x1");
+  x2 = subtree.get<float>("dim.<xmlattr>.x2");
+  y1 = subtree.get<float>("dim.<xmlattr>.y1");
+  y2 = subtree.get<float>("dim.<xmlattr>.y2");
+
+  agg_parent->elecs.push_back(std::make_shared<Electrode>(x1,x2,y1,y2,potential));
+
+  std::cout << "Electrode created with x1=" << agg_parent->elecs.back()->x1 << ", y1=" << agg_parent->elecs.back()->y1 <<
+    ", x2=" << agg_parent->elecs.back()->x2 << ", y2=" << agg_parent->elecs.back()->y2 <<
+    ", potential=" << agg_parent->elecs.back()->potential << std::endl;
+
+  return true;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//what used to be physeng
 void PhysicsConnector::helloWorld(void)
 {
   std::cout << eng_name << ", " << input_path << ", " << output_path << std::endl;
+}
+
+
+void PhysicsConnector::writeResultsXml()
+{
+  std::cout << "PhysicsConnector::writeResultsXML()" << std::endl;
+  // define major XML nodes
+  boost::property_tree::ptree tree;
+  boost::property_tree::ptree node_root;       // <sim_out>
+  boost::property_tree::ptree node_eng_info;   // <eng_info>
+  boost::property_tree::ptree node_sim_params; // <sim_params>
+  boost::property_tree::ptree node_electrode;  // <electrode>
+  boost::property_tree::ptree node_potential_map;  // <potential>
+
+  std::cout << "Write results to XML..." << std::endl;
+  // NOTE in the future, there's probably a range of stuff that can be exported.
+  // for now, only export charge config
+
+  // eng_info
+  node_eng_info.put("engine", "PoisSolver");
+  node_eng_info.put("version", "TBD"); // TODO real version
+
+  // sim_params
+  // TODO
+
+  // electrode
+  for (auto elec : elec_vec) {
+    boost::property_tree::ptree node_dim;
+    node_dim.put("<xmlattr>.x1", (std::to_string((elec.x[0]-SimParams::xoffset)/SimParams::finalscale/SimParams::Ls[0]).c_str()));
+    node_dim.put("<xmlattr>.y1", (std::to_string((elec.y[0]-SimParams::yoffset)/SimParams::finalscale/SimParams::Ls[1]).c_str()));
+    node_dim.put("<xmlattr>.x2", (std::to_string((elec.x[1]-SimParams::xoffset)/SimParams::finalscale/SimParams::Ls[0]).c_str()));
+    node_dim.put("<xmlattr>.y2", (std::to_string((elec.y[1]-SimParams::yoffset)/SimParams::finalscale/SimParams::Ls[1]).c_str()));
+    node_electrode.add_child("dim", node_dim);
+    boost::property_tree::ptree node_pot;
+    node_pot.put("", std::to_string(elec.potential).c_str());
+    node_electrode.add_child("potential", node_pot);
+  }
+
+  //potential_map
+  const int k = SimParams::ns[2]/2;
+  for (int i = 0; i < SimParams::ns[0]; i++){
+    for (int j = 0; j < SimParams::ns[1]; j++){
+      //create each entry
+      // std::cout << arr[SimParams::IND(i,j,k)] << ", " << std::to_string(arr[SimParams::IND(i,j,k)]).c_str() << std::endl;
+      boost::property_tree::ptree node_potential_val;
+      node_potential_val.put("<xmlattr>.x", (std::to_string((i*SimParams::ds[0]-SimParams::xoffset)/SimParams::finalscale/SimParams::Ls[0])).c_str());
+      node_potential_val.put("<xmlattr>.y", (std::to_string((j*SimParams::ds[1]-SimParams::yoffset)/SimParams::finalscale/SimParams::Ls[1])).c_str());
+      node_potential_val.put("<xmlattr>.val", (std::to_string(arr[SimParams::IND(i,j,k)]).c_str()));
+      node_potential_map.add_child("potential_val", node_potential_val);
+    }
+  }
+  node_root.add_child("eng_info", node_eng_info);
+  node_root.add_child("electrode", node_electrode);
+  node_root.add_child("potential_map", node_potential_map);
+  tree.add_child("sim_out", node_root);
+
+  // write to file
+  boost::property_tree::write_xml(output_path, tree, std::locale(), boost::property_tree::xml_writer_make_settings<std::string>(' ',4));
+
+  std::cout << "Write to XML complete." << std::endl;
 }
--- PoisFFT/src/phys_connector.h
+++ PoisFFT/src/phys_connector.h
@@ -16,9 +16,53 @@
 #include "problem.h"
 #include "electrodes.h"
 
+#include <stack>
+#include <memory>
+#include <map>
+#include <iostream>
+
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
 #include <string>
 #include <vector>
 #include <boost/circular_buffer.hpp>
+
+// Physical constants used for calculation
+namespace PhysConstants
+{
+   extern const double QE; //elementary charge
+   extern const double EPS0; //permittivity of free space
+   extern const double WF_GOLD; //workfunction for gold in eV
+   extern const double WF_COPPER; //workfunction for copper in eV
+   extern const double WF_ZINC; //workfunction for zinc in eV
+   extern const double WF_CESIUM; //workfunction for cesium in eV
+   extern const double WF_NICKEL; //workfunction for nickel in eV
+   extern const double CHI_SI; //electron affinity for silicon in eV from http://www.ioffe.ru/SVA/NSM/Semicond/Si/basic.html
+   extern const double EPS_SI; //relative permittivity of silicon
+};
+
+// Simulation parameters used for simulation control
+namespace SimParams
+{
+  // scaling and offset values
+  extern double finalscale, xoffset, yoffset;
+  extern std::string resultpath;
+
+  //stuff used during simulation
+  extern double Ls[3]; // simulation length in x, y, z
+  extern int ns[3]; // resolution in x, y, z
+  extern double ds[3]; // simulation length per resolution step, CALCULATED
+  extern int BCs[6]; // boundary conditions for left, right, top, bottom, front, back.
+  extern double MAX_ERROR;
+  extern int IND(int i, int j, int k);
+
+  //stuff used post-simulation
+  extern char* OUTFILE;
+  extern char* RHOFILE;
+  extern char* EPSFILE;
+  extern char* CORRFILE;
+  extern char* RESXML;
+};
 
 namespace phys{
   namespace bpt = boost::property_tree;
@@ -30,7 +74,94 @@ namespace phys{
     //DESTRUCTOR
     ~PhysicsConnector(){};
     void helloWorld(void);
+    void writeResultsXml();
+
+    // Constructor
+    void initProblem();
+
+    // File Handling
+    bool readProblem(const std::string &fname);
+
+    // Accessors
+    //! Checks if a parameter exists given the parameter key.
+    bool parameterExists(const std::string &key) {return sim_params.find(key) != sim_params.end();}
+    //! Getter for a parameter, given a parameter key.
+    std::string getParameter(const std::string &key) {return sim_params.find(key) != sim_params.end() ? sim_params.at(key) : "";}
+
+    //simulation inputs and outputs
+    std::vector<Electrodes> elec_vec;
+    double *arr;
+    std::vector<std::pair<float,float>> db_locs;
+    boost::circular_buffer<std::vector<int>> db_charges;
+
+    // STRUCTS
+    // electrode
+    struct Electrode {
+      float x1,x2,y1,y2;      // pixel location of electrode.
+      float potential;  // voltage that the electrode is set to
+      Electrode(float in_x1, float in_x2, float in_y1, float in_y2, float in_potential)
+        : x1(in_x1), x2(in_x2), y1(in_y1), y2(in_y2), potential(in_potential) {};
+    };
+
+    // aggregate
+    class Aggregate
+    {
+    public:
+      std::vector<std::shared_ptr<Aggregate>> aggs;
+      std::vector<std::shared_ptr<Electrode>> elecs;
+
+      // Properties
+      int size(); // returns the number of contained elecs, including those in children aggs
+    };
+
+    // ITERATOR
+    typedef std::vector<std::shared_ptr<Electrode>>::const_iterator ElecIter;
+    typedef std::vector<std::shared_ptr<Aggregate>>::const_iterator AggIter;
+
+    class ElecIterator
+    {
+    public:
+      explicit ElecIterator(std::shared_ptr<Aggregate> root, bool begin=true);
+
+      //~Iterator() {delete agg_stack;};
+
+      ElecIterator& operator++(); // recursive part here
+      bool operator==(const ElecIterator &other) {return other.elec_iter == elec_iter;}
+      bool operator!=(const ElecIterator &other) {return other.elec_iter != elec_iter;}
+      std::shared_ptr<Electrode> operator*() const {return *elec_iter;}
+    private:
+      ElecIter elec_iter;                   // points to the current electrode
+      std::shared_ptr<Aggregate> curr;  // current working Aggregate
+      std::stack<std::pair<std::shared_ptr<Aggregate>, AggIter>> agg_stack;
+      // add a new aggregate pair to the stack
+      void push(std::shared_ptr<Aggregate> agg);
+      // pop the aggregate stack
+      void pop();
+    };
+    ElecIterator begin() {return ElecIterator(elec_tree);}
+    ElecIterator end() {return ElecIterator(elec_tree, false);}
+
   private:
+
+    bool readProgramProp(const bpt::ptree &);
+    bool readMaterialProp(const bpt::ptree &);
+    bool readSimulationParam(const bpt::ptree &sim_params_tree);
+    bool readDesign(const bpt::ptree &subtree, const std::shared_ptr<Aggregate> &agg_parent);
+    bool readItemTree(const bpt::ptree &subtree, const std::shared_ptr<Aggregate> &agg_parent);
+    bool readElectrode(const bpt::ptree &subtree, const std::shared_ptr<Aggregate> &agg_parent);
+
+    // Variables
+    std::shared_ptr<Aggregate> elec_tree;
+    std::map<std::string, std::string> program_props;
+    // std::map<std::string, std::string> material_props; TODO probably need a different structure for this
+    std::map<std::string, std::string> sim_params;
+
+
+    ElecIter elec_iter;                   // points to the current electrode
+    std::shared_ptr<Aggregate> curr;  // current working Aggregate
+    std::stack<std::pair<std::shared_ptr<Aggregate>, AggIter>> agg_stack;
+
+
     std::string eng_name;
     std::string input_path;
     std::string output_path;
