2b10020e881ed500f79452808f08a644890db717
NathanChiu <nathan.chiu94@gmail.com>
2018-04-18 13:19:09 -0700

    clean FEM 3D a little

--- FEM/src/Poisson_3D.py
+++ FEM/src/Poisson_3D.py
@@ -1,7 +1,4 @@
-# from dolfin import *
 import dolfin
-import matplotlib.pyplot as plt
-import mshr
 import subprocess
 from paraview import simple as pvs
 import numpy as np
@@ -53,6 +50,7 @@ front = Front()
 back = Back()
 obstacle = Obstacle()
 obstacle2 = Obstacle2()
+print "Create subdomains..."
 
 # Define mesh
 domain = """
@@ -193,10 +191,8 @@ with open('../data/domain.geo', 'w') as f: f.write(domain)
 subprocess.call(['gmsh -3 ../data/domain.geo'], shell=True)
 subprocess.call(['dolfin-convert ../data/domain.msh domain.xml'], shell=True)
 mesh = dolfin.Mesh('domain.xml')
-
-# domain = mshr.Box(dolfin.Point(0,0,0), dolfin.Point(1,1,1))
-# mesh = mshr.generate_mesh(domain, 50)
 print "Mesh initialized"
+
 # Initialize mesh function for interior domains
 domains = dolfin.MeshFunction("size_t", mesh, mesh.topology().dim())
 domains.set_all(0)
@@ -227,15 +223,14 @@ g_Bo = dolfin.Constant("0.0")
 g_F = dolfin.Constant("0.0")
 g_Ba = dolfin.Constant("0.0")
 f = dolfin.Constant(-1.0E10*Q_E)
-
-print "Inputs finished"
+print "Boundary values created"
 
 # Define function space and basis functions
 V = dolfin.FunctionSpace(mesh, "CG", 3)
 u = dolfin.TrialFunction(V)
 v = dolfin.TestFunction(V)
-
 print "Function, space, basis defined"
+
 # Define Dirichlet boundary conditions at top, bottom, front, back
 # bcs = [dolfin.DirichletBC(V, 0.0, boundaries, 1),
 #        dolfin.DirichletBC(V, 0.0, boundaries, 3),
@@ -244,11 +239,10 @@ print "Function, space, basis defined"
 #        dolfin.DirichletBC(V, 1.0, boundaries, 7)]
 
 bcs = [dolfin.DirichletBC(V, 5.0, boundaries, 7)]
-
 print "Dirichlet boundaries defined"
+
 # Define new measures associated with the interior domains and
 # exterior boundaries
-
 dx = dolfin.dx(subdomain_data=domains)
 ds = dolfin.ds(subdomain_data=boundaries)
 print "Measures defined"
@@ -262,10 +256,13 @@ F = ( dolfin.inner(a0*dolfin.grad(u), dolfin.grad(v))*dx(0) \
     - g_F*v*ds(5) - g_Ba*v*ds(6) \
     - f*v*dx(0) - f*v*dx(1) ) #- f*v*dolfin.dx(2) )
 print "Variational Form defined"
+
 # Separate left and right hand sides of equation
 a, L = dolfin.lhs(F), dolfin.rhs(F)
 print "Separated LHS and RHS"
+
 # Solve problem
+print "Initializing solver parameters..."
 u = dolfin.Function(V)
 
 problem = dolfin.LinearVariationalProblem(a, L, u, bcs)
@@ -277,58 +274,28 @@ cg_param = solver.parameters['krylov_solver']
 cg_param['absolute_tolerance'] = 1E-7
 cg_param['relative_tolerance'] = 1E-4
 cg_param['maximum_iterations'] = 1000
+print "Solving problem..."
 solver.solve()
 # dolfin.parameters['krylov_solver']['maximum_iterations'] = 10
 # dolfin.solve(a == L, u, bcs, solver_parameters={'linear_solver': 'cg', 'preconditioner': 'ilu'})
 # solve(a == L, u, bcs, solver_parameters={"newton_solver":{"relative_tolerance":1e-6}})
 print "Solve finished"
-# Evaluate integral of normal gradient over top boundary
 
-# Create boundary mesh consisting of the 6 sides of the cube
-bmesh = dolfin.BoundaryMesh(mesh, "exterior")
-
-# Create SubMesh for side at z=0
-# This will be a UnitSquareMesh with topology dimension 2 in 3 space dimensions
-cc = dolfin.MeshFunction("size_t", bmesh, bmesh.topology().dim())
-# cc = CellFunction('size_t', bmesh, 0)
-xyplane = dolfin.AutoSubDomain(lambda x: x[2] < 1e-8)
-xyplane.mark(cc, 1)
-submesh = dolfin.SubMesh(bmesh, cc, 1)
-
-# Move slice/submesh to z=0.5
-slice_height = 0.5
-x = submesh.coordinates()
-x[:, 2] += slice_height
-
-# plt.figure()
-# plot(submesh, title="submesh")
-
-# Create a FunctionSpace on the submesh
-Vs = dolfin.FunctionSpace(submesh, "CG", 1)
-u.set_allow_extrapolation(True)
-us = dolfin.interpolate(u, Vs)
-
-plt.figure()
-dolfin.plot(us, title="slice")
-
-plt.figure()
-dolfin.plot(dolfin.grad(us), title="Projected grad(us)")
-
-print "Print solution to file."
+# PRINT TO FILE
+print "Saving solution to .pvd file..."
 file_string = "../data/Potential.pvd"
 dolfin.File(file_string) << u
-
+print "Saving to .csv file..."
 reader = pvs.OpenDataFile("../data/Potential.pvd")
 writer = pvs.CreateWriter("../data/Potential.csv", reader)
 writer.WriteAllTimeSteps = 1
 writer.FieldAssociation = "Points"
 writer.UpdatePipeline()
-print "CSV created"
+print "Solution saved."
 
-plt.show()
 ##PLOTTING
+print "Starting paraview..."
 data_3d = pvs.PVDReader(FileName=file_string)
-
 #create the slice
 yslice = pvs.Slice(Input=data_3d, SliceType="Plane")  
 yslice.SliceType.Origin = [0, 0.5, 0]
@@ -344,3 +311,5 @@ pvs.Show(zslice)
 pvs.Show(yslice)
 pvs.Interact(view=None)
 pvs.Render()
+
+print "Ending."
