329f5cf373270c7782f0ca7af532c890aa403522
Nathan Chiu <nathan.chiu94@gmail.com>
2019-02-08 15:13:07 -0800

    Change dopant.py to use np.gradien() instead of creating a finite difference matrix.

--- FEM/src/python/dopant.py
+++ FEM/src/python/dopant.py
@@ -1,6 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def plot(x, y, title, x_label, y_label, file_name):
+    plt.clf()
+    plt.plot(x,y)
+    plt.xlabel(x_label) #set label text
+    plt.ylabel(y_label)
+    plt.title(title)
+
+    locs, labels = plt.xticks()
+    labels = []
+    for loc in locs:
+        labels += [str(int(round(loc/1E-7, 2)))]
+    plt.xticks(locs, labels)
+
+    # plt.savefig("n.pdf")
+    plt.savefig("{}{}".format(file_name, ".pdf"))
+
 # Physical constants
 q = 1.60E-19 # Elementary charge - Coulomb
 eps_0 = 8.85E-12 # Absolute permittivity - Farad / metre
@@ -8,7 +24,7 @@ k = 1.380E-23 # Boltzmann constant - metre^2 kilogram / second^2 Kelvin
 T = 293 # Temperature - Kelvin
 
 # Simulation parameters
-resolution = 10
+resolution = 200
 eps_r = 11.9
 eps = eps_r*eps_0
 
@@ -40,14 +56,8 @@ n_ext = profile*dopant
 # Assume full ionization
 n = n_ext + n_int
 
-# Construct finite difference derivative matrix
-ddx = -np.diag(np.ones(resolution)) + np.diag(np.ones(resolution-1), 1)
-ddx[resolution-1][resolution-2] = -1
-ddx[resolution-1][resolution-1] = 1
-ddx = ddx/(x[1]-x[0])
-
 # Obtain derivative of electron density
-dndx = np.dot(ddx,n)
+dndx = np.gradient(n,x)
 
 # Find the electric field that results in a drift current
 # which exactly balances diffusion current
@@ -55,27 +65,8 @@ E = -k*T/q/n*dndx
 
 # Resulting spatial charge density
 # Can be obtained by taking another derivative of E, and scaling by eps.
-rho = np.dot(ddx,E)
+rho = np.gradient(E, x)*eps
 
-plt.plot(x,n)
-plt.xlabel("X (um)") #set label text
-plt.ylabel("Electron density (m^-3)")
-plt.title('Electron density')
-
-locs, labels = plt.xticks()
-labels = []
-for loc in locs:
-    labels += [str(int(round(loc/1E-7, 2)))]
-plt.xticks(locs, labels)
-
-plt.savefig("n.pdf")
-# print(E)
-# print(rho)
-# plt.plot(x,rho)
-# plt.show()
-# print(E)
-# print(dndx)
-
-# plt.plot(x, n_ext)
-# plt.show()
-# print(ddx)
+plot(x, n, "Electron Density", "X (um)", "Electron density (m^-3)", "n")
+plot(x, E, "Electric Field", "X (um)", "Electric field (Vm^-1)", "E")
+plot(x, rho, "Charge Density", "X (um)", "Charge density (m^-3)", "rho")
