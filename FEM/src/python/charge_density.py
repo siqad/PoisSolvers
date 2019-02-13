6d2dd93f1b425f81f963546595d69e859875b1ef
Nathan Chiu <nathan.chiu94@gmail.com>
2019-02-13 12:04:14 -0800

    Add in charge_density.py, used to define a dolfin Expression that represents the charge density obtained by dopant.py

---
+++ FEM/src/python/charge_density.py
@@ -0,0 +1,44 @@
+import numpy as np
+import dolfin as df
+import matplotlib.pyplot as plt
+
+class ChargeDensity(df.Expression):
+  def __init__(self, func_in, **kwargs):
+    self.data = kwargs["data"]
+    self.x = kwargs["x"]
+    self.F = func_in
+
+
+  def eval(self, val, x):
+    f_val = self.F(x) # This should be the that goes into the charge density
+                      # function. E.g. x[0], or x[1], etc.
+    # interpolate the data at the point of evaluation, since the
+    val[0] = np.interp(f_val, self.x, self.data)
+
+  # If the expression is vector valued, overload this.
+  # def value_shape(self):
+  #   return (1,)
+
+if __name__ == "__main__":
+    rho = np.array([-8.54714149e-07, -8.33732142e-05, -1.60989631e-02, -3.10772450e+00,
+     -5.70687012e+02, -1.05369927e+04, -1.10135228e+04, -2.80024598e+02,
+      1.07501397e+04,  1.07601003e+04,  8.30053469e+02,  6.00024250e+01,
+      4.03255519e+00,  2.23706823e-02,  1.15898807e-04,  6.00232312e-07,
+      3.10857530e-09,  1.61008849e-11,  6.83205865e-14,  0.00000000e+00])
+    # rho = rho / abs(max(rho))
+    # print(rho)
+    x = np.array([-1.00000000e-06, -8.94736842e-07, -7.89473684e-07, -6.84210526e-07,
+     -5.78947368e-07, -4.73684211e-07, -3.68421053e-07, -2.63157895e-07,
+     -1.57894737e-07, -5.26315789e-08,  5.26315789e-08,  1.57894737e-07,
+      2.63157895e-07,  3.68421053e-07,  4.73684211e-07,  5.78947368e-07,
+      6.84210526e-07,  7.89473684e-07,  8.94736842e-07,  1.00000000e-06])
+
+    # mesh = df.UnitSquareMesh(64, 64)
+    print(min(x), max(x))
+    mesh = df.RectangleMesh(df.Point(min(x), min(x)), df.Point(max(x), max(x)), 64, 64)
+    V = df.FunctionSpace(mesh, 'CG', 1)
+    F = df.interpolate(df.Expression('x[0]',  degree=1), V)
+    rho_exp = df.interpolate(ChargeDensity(F, data=rho,x=x, degree=1), V)
+
+    df.plot(rho_exp)
+    plt.show()
--- FEM/src/python/dopant.py
+++ FEM/src/python/dopant.py
@@ -1,5 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+import dolfin as df
+from charge_density import ChargeDensity
 
 def plot(x, y, title, x_label, y_label, file_name):
     plt.clf()
@@ -7,14 +10,6 @@ def plot(x, y, title, x_label, y_label, file_name):
     plt.xlabel(x_label) #set label text
     plt.ylabel(y_label)
     plt.title(title)
-
-    locs, labels = plt.xticks()
-    labels = []
-    for loc in locs:
-        labels += [str(int(round(loc/1E-7, 2)))]
-    plt.xticks(locs, labels)
-
-    # plt.savefig("n.pdf")
     plt.savefig("{}{}".format(file_name, ".pdf"))
 
 # Physical constants
@@ -24,7 +19,7 @@ k = 1.380E-23 # Boltzmann constant - metre^2 kilogram / second^2 Kelvin
 
 # Simulation parameters
 T = 293 # Temperature - Kelvin
-resolution = 200
+resolution = 20
 eps_r = 11.9
 eps = eps_r*eps_0
 
@@ -32,8 +27,8 @@ ni_si = 1E10 # in cm^-3
 ni_si = ni_si*1E2*1E2*1E2 # conversion to metre^-3
 
 # Spatial extent
-x_min = -1E-6
-x_max = 1E-6
+x_min = -1E-7
+x_max = 1E-7
 x = np.linspace(x_min, x_max, resolution)
 
 # Intrinsic electron density
@@ -45,12 +40,14 @@ dopant = 1E19 # in cm^-3
 dopant = dopant*1E2*1E2*1E2
 
 # Scaling and offset for sigmoid
-x_offset = 0
-x_scaling = 50
+x_offset = -50E-9
+x_scaling = 100
 x_arg = x_scaling*(x-x_offset)/x_max
 
 #Creating the doping profile
-profile = 1/(1+np.exp(-x_arg))
+profile = 1 - 1/(1+np.exp(-x_arg))
+# profile_pw = np.piecewise(x, [x < 5E-7, x>=5E-7], [lambda x: 1/(1+np.exp(-x_scaling*(x-x_offset)/x_max)), 0])
+# profile_pw = np.piecewise(x, [x < 5E-7, x>=5E-7], [lambda x: 1/(1+np.exp(-x_scaling*(x-x_offset)/x_max)), 0])
 n_ext = profile*dopant
 
 # Total electron density (as a fuction of x)
@@ -64,10 +61,24 @@ dndx = np.gradient(n,x)
 # which exactly balances diffusion current
 E = -k*T/q/n*dndx
 
+# print(n)
 # Resulting spatial charge density
 # Can be obtained by taking another derivative of E, and scaling by eps.
 rho = np.gradient(E, x)*eps
 
-plot(x, n, "Electron Density", "X (um)", "Electron density (m^-3)", "n")
-plot(x, E, "Electric Field", "X (um)", "Electric field (Vm^-1)", "E")
-plot(x, rho, "Charge Density", "X (um)", "Charge density (m^-3)", "rho")
+# Plot what we have so far
+# fig, ax = plt.subplots()
+# plot(x, n, "Electron Density", "X (m)", "Electron density (m^-3)", "n")
+# plot(x, E, "Electric Field", "X (m)", "Electric field (Vm^-1)", "E")
+# plot(x, rho, "Charge Density", "X (m)", "Charge density (m^-3)", "rho")
+
+# Create a general mesh
+mesh = df.RectangleMesh(df.Point(min(x), min(x)), df.Point(max(x), max(x)), 64, 64)
+# Possible functions that can live on this mesh
+V = df.FunctionSpace(mesh, 'CG', 1)
+#Get the parameter
+F = df.interpolate(df.Expression('x[0]',  degree=1), V)
+rho_exp = df.interpolate(ChargeDensity(F, data=rho,x=x, degree=1), V)
+
+df.plot(rho_exp)
+plt.show()
