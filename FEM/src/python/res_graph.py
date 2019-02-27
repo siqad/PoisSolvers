49829c97c92b4528a8075416e3b26b27403c4d3a
Nathan Chiu <nathan.chiu94@gmail.com>
2019-02-27 14:18:04 -0800

    Graph connectivity finished, need to make edges reflect resistance.

--- FEM/src/python/res_graph.py
+++ FEM/src/python/res_graph.py
@@ -25,44 +25,55 @@ class ResGraph():
         self.z_bounds.append(max(elec.z2 for elec in self.elec_list))
         print(self.z_bounds)
 
-    def checkOverlap(self, pair):
-        a = pair[0]
-        b = pair[1]
+    def addFromOverlap(self, pair):
+        a, b = pair
 
         #if any of these are true, no overlap
         if a.x1 > b.x2 or a.x2 < b.x1 \
             or a.y1 > b.y2 or a.y2 < b.y1 \
             or a.z1 > b.z2 or a.z2 < b.z1:
-            print("No overlap!")
+            pass
+
+        #Overlap exists between electrodes, this pair is connected.
+        #Find which coordinate overlaps, ignore rotated ones for now.
         else:
-            #Overlap exists between electrodes, this pair is connected.
-            print("Overlap!")
-            #Find which coordinate overlaps, ignore rotated ones for now.
-            if a.x1 == b.x2 or a.x2 == b.x1:
-                #y and z coordinates form the intersection
-                print("1")
-            if a.y1 == b.y2 or a.y2 == b.y1:
-                print("2")
+            #y and z coordinates form the intersection
+            if a.x1 == b.x2:
+                box_a = sp.box(a.y1, a.z1, a.y2, a.z2)
+                box_b = sp.box(b.y1, b.z1, b.y2, b.z2)
+                inter = box_a.intersection(box_b)
+                pos = [a.x1, inter.centroid.x, inter.centroid.y]
+            if a.x2 == b.x1:
+                box_a = sp.box(a.y1, a.z1, a.y2, a.z2)
+                box_b = sp.box(b.y1, b.z1, b.y2, b.z2)
+                inter = box_a.intersection(box_b)
+                pos = [a.x2, inter.centroid.x, inter.centroid.y]
+
+            # x and z coordinates form the intersection
+            if a.y1 == b.y2:
+                box_a = sp.box(a.x1, a.z1, a.x2, a.z2)
+                box_b = sp.box(b.x1, b.z1, b.x2, b.z2)
+                inter = box_a.intersection(box_b)
+                pos = [inter.centroid.x, a.y1, inter.centroid.y]
+            if a.y2 == b.y1:
+                box_a = sp.box(a.x1, a.z1, a.x2, a.z2)
+                box_b = sp.box(b.x1, b.z1, b.x2, b.z2)
+                inter = box_a.intersection(box_b)
+                pos = [inter.centroid.x, a.y2, inter.centroid.y]
+
             #x and y coordinates form the intersection
             if a.z1 == b.z2:
-                print("3")
                 box_a = sp.box(a.x1, a.y1, a.x2, a.y2)
                 box_b = sp.box(b.x1, b.y1, b.x2, b.y2)
                 inter = box_a.intersection(box_b)
                 pos = [inter.centroid.x, inter.centroid.x, a.z1]
-                # curr_node = self.addNode(pos=[center.x, center.y,a.z1])
             if a.z2 == b.z1:
-                print("4")
                 box_a = sp.box(a.x1, a.y1, a.x2, a.y2)
                 box_b = sp.box(b.x1, b.y1, b.x2, b.y2)
                 inter = box_a.intersection(box_b)
                 pos = [inter.centroid.x, inter.centroid.x, a.z2]
             includes = [a.id, b.id]
-            curr_node = self.addNode(pos=pos, includes=includes)
-            # self.addEdge(a.id, b.id)
-
-            #Add a node centered at the electrode face.
-            # self.g.addNode
+            curr_node = self.addNode(pos=pos, includes=includes, end=False)
 
     # Add in the nodes that connect to the ceiling
     def addCeilingNodes(self, net_elec):
@@ -73,7 +84,7 @@ class ResGraph():
                 box = sp.box(item.x1, item.y1, item.x2, item.y2)
                 center = box.centroid
                 #add its center
-                curr_node = self.addNode(pos=[center.x, center.y,item.z2], includes=[item.id])
+                curr_node = self.addNode(pos=[center.x, center.y,item.z2], includes=[item.id], end=False)
                 self.addEdge(curr_node, "ceiling")
 
     def addFloorNodes(self, net_elec):
@@ -84,7 +95,7 @@ class ResGraph():
                 box = sp.box(item.x1, item.y1, item.x2, item.y2)
                 center = box.centroid
                 #add its center
-                curr_node = self.addNode(pos=[center.x, center.y,item.z1], includes=[item.id])
+                curr_node = self.addNode(pos=[center.x, center.y,item.z1], includes=[item.id], end=False)
                 self.addEdge(curr_node, "floor")
 
     # Adds an edge to the graph between two nodes a and b, where a and b are node keys.
@@ -105,7 +116,7 @@ class ResGraph():
         pairs = itertools.combinations(self.elec_dict[key], 2)
         # Check for overlap
         for pair in pairs:
-            self.checkOverlap(pair)
+            self.addFromOverlap(pair)
 
     #gets the nodes that the electrode with a given id is involved with
     def getRelevantNodes(self, id):
@@ -115,44 +126,67 @@ class ResGraph():
                 relevant_nodes.append(node[0])
         return relevant_nodes
 
-
-    def getDistanceDict(self, subgraph):
-        # node_pos = self.g.nodes(data='pos')
-        # subgraph = self.g.subgraph(rel_nodes)
+    def getDistanceDict(self, subgraph, rel_nodes):
         node_pos = subgraph.nodes(data='pos')
-        # pairs = itertools.combinations(rel_nodes, 2)
         pairs = [x for x in itertools.combinations(rel_nodes, 2) ]
         distances = []
 
         for pair in pairs:
-            a = pair[0]
-            b = pair[1]
+            a, b = pair
             point_a = np.array(node_pos[a])
             point_b = np.array(node_pos[b])
             #Get the distance between node positions
             distances.append(np.linalg.norm(point_a - point_b))
-
-        # print(pairs)
-        # print(distances)
         return dict(zip(pairs, distances))
 
-    def connectSubgraph(self):
-        pass
+    # Connect each node to its two nearest neighbours. End nodes are connected only to a single nearest neighbour.
+    def connectSubgraph(self, sg, dist_dict):
+        for node, attributes in sg.nodes(data=True):
+            #for each entry in the dist
+            for pair in dist_dict:
+                #Only about the pair if it includes the current node of interest
+                if node in pair:
+                    # print(pair, node)
+                    #get the relevant pair keys in ascending order.
+                    sorted_pairs = sorted(dist_dict, key=lambda x: (dist_dict[x]))
+                    sorted_filtered_pairs = list(filter(lambda x: node in x, sorted_pairs))
+                    if attributes["end"] == True:
+                        #End item, connect only the closest one.
+                        a, b = sorted_filtered_pairs[0]
+                        self.addEdge(a, b)
+                        print("Connecting: ", a, b)
+                    else:
+                        #Connect the two closest
+                        for i in range(2):
+                            a, b = sorted_filtered_pairs[i]
+                            self.addEdge(a, b)
+                            print("Connecting: ", a, b)
+
+    #mark the two nodes farthest from each other as being the ends
+    def markMaxDist(self, dist_dict, sg):
+        max_key = max(dist_dict, key=lambda x: dist_dict[x])
+        # max_key should be a list with two node keys
+        for node in sg.nodes():
+            print("Node: ", node, "max_key: ", max_key)
+            if node in max_key:
+                self.addNode(node, end=True)
+                print("Setting node ", node, " as end node.")
+            # else:
+                # self.addNode(node, end=False)
+        # for key in max_key:
+        #     self.addNode(key, end=True)
 
     #edges are defined by the electrode geometries
     def buildEdges(self):
         for item in self.elec_list:
             rel_nodes = self.getRelevantNodes(item.id)
-            subgraph = self.g.subgraph(rel_nodes)
-            self.connectSubgraph(subgraph)
-            print(subgraph.degree())
-            # dist_dict = self.getDistanceDict(relevant_nodes)
-            #nodes now sorted by distance
+            sg = self.g.subgraph(rel_nodes)
+            dist_dict = self.getDistanceDict(sg, rel_nodes)
             # print(dist_dict)
-
-
-
-
+            self.markMaxDist(dist_dict, sg)
+            self.connectSubgraph(sg, dist_dict)
+            #reset the end attribute for all nodes
+            nx.set_node_attributes(sg, False, "end")
 
     def buildGraph(self):
         self.g = nx.Graph()
@@ -175,8 +209,6 @@ class ResGraph():
     def exportGraph(self):
         plt.figure()
         nx.draw(self.g, with_labels=True)
-        # file_name = self.dir+"graph.pdf"
-        # print(file_name)
         plt.savefig(self.dir+"/graph.pdf", bbox_inches='tight')
 
     def debugPrint(self):
